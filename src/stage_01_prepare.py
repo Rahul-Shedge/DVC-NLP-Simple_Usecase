cd9929d30723f43c37746db3840d010d16bb9857
Rahul-Shedge <rahulshedge555@outlook.com>
2023-01-18 01:18:09 +0530

    stage 01 directory created

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -22,7 +22,19 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+
+    source_data = config["source_data"]
+    input_data = os.path.join(source_data["dir"],source_data["data_file"])
+
+    split = params["prepare"]["split"]
+    seed = params["prepare"]["seed"]
+    random.seed(seed)
+
+    artifacts = config["artifacts"]
+    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
+
+    create_directories([prepared_data_dir_path])
+
 
 
 if __name__ == '__main__':
