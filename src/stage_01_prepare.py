ede29b86a7336af1ca15ece3ad205ac143a8d0aa
Rahul-Shedge <rahulshedge555@outlook.com>
2023-01-18 01:25:30 +0530

    stage 01 tsv empty files created

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -24,7 +24,7 @@ def main(config_path, params_path):
     params = read_yaml(params_path)
 
     source_data = config["source_data"]
-    input_data = os.path.join(source_data["dir"],source_data["data_file"])
+    input_data = os.path.join(source_data["data_dir"],source_data["data_file"])
 
     split = params["prepare"]["split"]
     seed = params["prepare"]["seed"]
@@ -34,6 +34,15 @@ def main(config_path, params_path):
     prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
 
     create_directories([prepared_data_dir_path])
+    train_data_path = os.path.join(prepared_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepared_data_dir_path,artifacts["TEST_DATA"])
+    
+    encode = "utf8"
+    with open(input_data,encoding=encode) as fd_in:
+        with open(train_data_path,"w",encoding=encode) as fd_out_train:
+            with open(test_data_path,"w",encoding=encode) as fd_out_test:
+                pass 
+
 
 
 
