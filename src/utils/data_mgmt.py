c8f833f016acf08922c1498ff2df72c08a22ba7e
Rahul-Shedge <rahulshedge555@outlook.com>
2023-01-21 09:50:33 +0530

    stage 01 mistake has been corrected

--- src/utils/data_mgmt.py
+++ src/utils/data_mgmt.py
@@ -19,7 +19,7 @@ def process_posts(fd_in,fd_out_train,fd_out_test,target_tag,split):
             body =  re.sub(r"\s+"," ",attr.get("Body",""))
             text = title + " " + body    
 
-            fd_out.write(f"{pid}\t{label}\t{text}\t{text}\n") 
+            fd_out.write(f"{pid}\t{label}\t{text}\n") 
             line_num += 1
         except Exception as e:
             msg = f"Skipping the broken line {line_num} as {e}\n"
